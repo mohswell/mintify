ccce582b52a860302d5880cf7ac1c360deb0a513
Muhammad Said <mohammedabdy10@gmail.com>
2024-11-06 16:42:53 +0300

    bootstrapped nest app with the pipe and compression json objects

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { validateConfig } from './config/validate.config';
+import { env } from './config/env.config';
+import * as express from 'express';
+import * as compression from 'compression';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+function setupSwagger(app: NestExpressApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Gemini API Example')
+    .setDescription('API Documentation for Gemini integration')
+    .setVersion('1.0')
+    .addTag('gemini')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 4000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(validateConfig);
+  app.use(express.json({ limit: '1000kb' }));
+  app.use(express.urlencoded({ extended: false }));
+  app.use(compression());
+  setupSwagger(app);
+  await app.listen(env.PORT);
 }
 bootstrap();
