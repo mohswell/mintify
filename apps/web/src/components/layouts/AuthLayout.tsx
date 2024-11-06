94629d013eeb90d8282b7dc39b0783c3e54b4305
Muhammad Said <mohammedabdy10@gmail.com>
2024-11-06 15:29:20 +0300

    add dark mode to login page

--- apps/web/src/components/layouts/AuthLayout.tsx
+++ apps/web/src/components/layouts/AuthLayout.tsx
@@ -1,21 +1,27 @@
 import { LayoutProps } from "@/types";
 import Slides from "@/components/Slides";
 import { AppLayout } from "@/components/layouts";
+import { ThemeProvider } from "next-themes";
+import Providers from "@/components/Providers";
 
 export function AuthLayout({ page, children }: LayoutProps) {
   return (
-    <AppLayout page={page} className="!bg-white !text-dark">
-      <div className="w-full flex-1 flex flex-col lg:flex-row lg:justify-center justify-start lg:items-center items-start">
-        <div className="grid lg:grid-cols-2 grid-cols-1 w-full gap-12">
-          <div className="w-full overflow-hidden relative bg-black/10 md:h-[412px] h-[312px]">
-            <Slides />
-          </div>
+    <ThemeProvider defaultTheme="light" attribute="class">
+      <Providers>
+        <AppLayout page={page} className="bg-white dark:bg-dark text-dark dark:text-white">
+          <div className="w-full flex-1 flex flex-col lg:flex-row lg:justify-center justify-start lg:items-center items-start">
+            <div className="grid lg:grid-cols-2 grid-cols-1 w-full gap-12">
+              <div className="w-full overflow-hidden relative bg-light-gray-2 dark:bg-dark-2 md:h-[412px] h-[312px]">
+                <Slides />
+              </div>
 
-          <div className="flex flex-col items-start w-full justify-center lg:min-h-[412px]">
-            {children}
+              <div className="flex flex-col items-start w-full justify-center lg:min-h-[412px]">
+                {children}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </AppLayout>
+        </AppLayout>
+      </Providers>
+    </ThemeProvider>
   );
 }
