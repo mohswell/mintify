8b118d1cd8ef46e4ca4c6ae1ae911a78187bd8c8
Muhammad Said <mohammedabdy10@gmail.com>
2024-11-06 15:30:01 +0300

    add toggle to footer

--- apps/web/src/components/Footer.tsx
+++ apps/web/src/components/Footer.tsx
@@ -9,11 +9,9 @@ export default function Footer() {
   return (
     <div className={`w-full px-4 md:px-[3%] md:py-6 py-4 flex justify-center items-center ${!isDashboard ? 'mt-24 md:mt-0' : ''}`}>
       <div className="w-full max-w-screen-11xl mx-auto flex md:justify-center justify-between items-center relative">
-        {isDashboard && (
           <div className="md:absolute relative md:left-0">
             <ThemeToggle />
           </div>
-        )}
 
         <span className="font-light md:text-sm text-xs !leading-[14px] tracking-[-2%] text-center">
           {new Date().getFullYear()} © PHOTORUUM FACILITY
