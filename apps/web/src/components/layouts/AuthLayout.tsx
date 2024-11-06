b7ceb1aa63d18fb1240a9805ba9ec3fe34d69924
Muhammad Said <mohammedabdy10@gmail.com>
2024-11-06 15:51:08 +0300

    remove slides

--- apps/web/src/components/layouts/AuthLayout.tsx
+++ apps/web/src/components/layouts/AuthLayout.tsx
@@ -1,5 +1,5 @@
 import { LayoutProps } from "@/types";
-import Slides from "@/components/Slides";
+// import Slides from "@/components/Slides";
 import { AppLayout } from "@/components/layouts";
 import { ThemeProvider } from "next-themes";
 import Providers from "@/components/Providers";
@@ -9,14 +9,18 @@ export function AuthLayout({ page, children }: LayoutProps) {
     <ThemeProvider defaultTheme="light" attribute="class">
       <Providers>
         <AppLayout page={page} className="bg-white dark:bg-dark text-dark dark:text-white">
-          <div className="w-full flex-1 flex flex-col lg:flex-row lg:justify-center justify-start lg:items-center items-start">
-            <div className="grid lg:grid-cols-2 grid-cols-1 w-full gap-12">
-              <div className="w-full overflow-hidden relative bg-light-gray-2 dark:bg-dark-2 md:h-[412px] h-[312px]">
+          <div className="w-full flex-1 flex flex-col lg:flex-row lg:justify-center justify-center lg:items-center items-center">
+            <div className="grid lg:grid-cols-1 grid-cols-1 w-full gap-12">
+              {/* Commenting out the Slides component */}
+              {/* <div className="w-full overflow-hidden relative bg-light-gray-2 dark:bg-dark-2 md:h-[412px] h-[312px]">
                 <Slides />
-              </div>
+              </div> */}
 
-              <div className="flex flex-col items-start w-full justify-center lg:min-h-[412px]">
-                {children}
+              {/* Centering and enlarging the children */}
+              <div className="flex flex-col items-center justify-center w-full lg:min-h-[500px]">
+                <div className="flex flex-col items-center justify-center w-full max-w-md lg:max-w-xl p-8 lg:p-16">
+                  {children}
+                </div>
               </div>
             </div>
           </div>
