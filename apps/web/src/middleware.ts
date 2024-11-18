655935f8a9f88b4096ecacad6d07ee1518a8d448
Muhammad Said <mohammedabdy10@gmail.com>
2024-11-18 17:08:26 +0300

    feat: update middleware to handle authentication and redirection; simplify auth page checks and logout logic

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -4,52 +4,44 @@ import { SESSION_NAME } from "@/lib/constants";
 import { decodeJWT } from "@/lib/utils";
 
 export function middleware(request: NextRequest) {
-  const { pathname, searchParams } = request.nextUrl;
+  const { pathname } = request.nextUrl;
 
-  const authPages = [
-    "/login",
-    "/verify",
-    "/forgot-password",
-    "/password-reset",
-  ];
+  const authPages = ["/"];
 
   const isAuthPage = authPages.includes(pathname);
-  const isSignupPage = pathname === "/signup";
 
   // Get the token from the session storage
   const token = request.cookies.get(SESSION_NAME)?.value;
 
   const handleLogout = () => {
-    const response = NextResponse.redirect(new URL("/login", request.url));
+    const response = NextResponse.redirect(new URL("/", request.url));
     response.cookies.delete(SESSION_NAME);
 
     return response;
   };
 
   if (token) {
-    // Decode the token
-    const decodedToken = decodeJWT(token);
+    try{
+      const decodedToken = decodeJWT(token);
 
-    // Check if the token has expired
-    if (
-      decodedToken &&
-      decodedToken.exp &&
-      Date.now() >= decodedToken.exp * 1000
-    ) {
-      // Log the user out if the token has expired
+      // Check if the token has expired
+      if (
+        decodedToken &&
+        decodedToken.exp &&
+        Date.now() >= decodedToken.exp * 1000
+      ) {
+        // Log the user out if the token has expired
+        return handleLogout();
+      }
+
+      if (isAuthPage) {
+        // If user is already logged in and tries to access auth pages, redirect to homepage
+        return NextResponse.redirect(new URL("/home", request.url));
+      }
+    } catch(error){
       return handleLogout();
     }
-
-    if (isAuthPage) {
-      // If user is already logged in and tries to access auth pages, redirect to dashboard
-      return NextResponse.redirect(new URL("/dashboard", request.url));
-    }
   } else if (!isAuthPage) {
-    // No token and trying to access a protected route, but allow signup with valid token
-    if (isSignupPage && searchParams.has("token")) {
-      // Allow access to the signup page if a valid token is present in the URL
-      return NextResponse.next();
-    }
     return handleLogout();
   }
 
