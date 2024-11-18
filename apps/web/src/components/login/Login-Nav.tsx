fa4f9a2fcda0a1f0e6c43dab84f35b3b0d5e841f
Muhammad Said <mohammedabdy10@gmail.com>
2024-11-18 17:17:05 +0300

    feat: add login components including Footer, Nav, Header, and MobileNav with reusable UI elements

---
+++ apps/web/src/components/login/Login-Nav.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import React from "react";
+import { LogoFull } from "@/components/ui/LogoFull";
+
+export function Nav() {
+  const pathname = usePathname();
+
+  return (
+    <>
+      <nav className="flex items-center justify-between px-4 md:px-6 lg:px-8 py-4">
+        <div className="container mx-auto flex justify-between items-center w-full">
+          {/* Logo for both mobile and desktop */}
+          <Link href="/" className="flex items-left justify-start">
+            <LogoFull />
+          </Link>
+
+          {/* Admin Access text: Centered on desktop, hidden on mobile */}
+          <div className="hidden md:flex items-center justify-center flex-grow mr-48">
+            <span className="font-kanit text-black dark:text-white text-[22px] font-medium leading-[10px] tracking-[-0.02em]">
+              Admin Access
+            </span>
+          </div>
+        </div>
+      </nav>
+      <hr className="border-t border-gray-300 mt-0 dark:bg-gray-500" />
+    </>
+  );
+}
