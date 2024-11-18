5e16877c2cec73bcf9e42d99b3cdc416868496a8
Muhammad Said <mohammedabdy10@gmail.com>
2024-11-18 17:07:40 +0300

    feat: implement user fetching hook and server-side user management; update constants and utility functions

--- apps/web/src/lib/constants.ts
+++ apps/web/src/lib/constants.ts
@@ -1,34 +1,5 @@
-import { SubmissionStatus } from "@/types";
+export const DEFAULT_PAGE = 1;
 
-export enum Device {
-  LAPTOP_4K = 2560,
-  LAPTOP_2XL = 1680,
-  LAPTOP_XL = 1512,
-  LAPTOP_LG = 1440,
-  LAPTOP = 1230,
-  TABLET_LG = 1196,
-  TABLET = 860,
-  TABLET_SM = 580,
-  MOBILE = 430,
-  MOBILE_SM = 375,
-}
+export const DEFAULT_TOTAL_ITEMS = 7;
 
-export const MAX_IMAGE_SIZE = 10;
-
-export const ITEMS_PER_PAGE = 6;
-
-export const STATUS_SYMBOLS: Record<SubmissionStatus, string> = {
-  pending: "⏳",
-  approved: "👍🏾",
-  rejected: "❌",
-};
-
-export const SESSION_NAME = "stucruum-session";
-
-export const SUBMISSION_FILTERS: { label: string; value: string }[] = [
-  { label: "All", value: "all" },
-  { label: "Art", value: "art" },
-  { label: "Pending", value: "pending" },
-  { label: "Approved", value: "approved" },
-  { label: "Rejected", value: "rejected" },
-];
+export const SESSION_NAME = "mintify-session"; 
--- apps/web/src/lib/helpers.ts
+++ apps/web/src/lib/helpers.ts
@@ -1,52 +1,22 @@
-export function validateEmail(email: string): boolean {
-  // Regular expression for email validation
-  const emailRegex: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-  return emailRegex.test(email);
-}
-
-export function convertImageSize(kb: number): string {
-  const mb = kb / 1024;
-  const gb = mb / 1024;
-
-  if (gb >= 1) {
-    return `${gb.toFixed(2)} GB`;
-  } else if (mb >= 1) {
-    return `${mb.toFixed(2)} MB`;
-  } else {
-    return `${kb.toFixed(2)} KB`;
-  }
-}
-
 export function assertValue<T>(v: T | undefined, errorMessage: string): T {
-  if (v === undefined) {
-    throw new Error(errorMessage);
-  }
+    if (v === undefined) {
+        throw new Error(errorMessage);
+    }
 
-  return v;
+    return v;
 }
 
-/**
- * Converts the file size to the specified unit and returns the size as a number.
- *
- * @param {File} file - The file whose size is to be checked.
- * @param {'bytes' | 'kb' | 'mb' | 'gb'} unit - The unit for the file size.
- * @return {number} The size of the file in the specified unit.
- */
-export function getFileSize(
-  file: File,
-  unit: "bytes" | "kb" | "mb" | "gb" = "kb",
-): number {
-  const sizeInBytes = file.size;
+export function validateEmail(email: string): boolean {
+    // Regular expression for email validation
+    const emailRegex: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  switch (unit) {
-    case "kb":
-      return sizeInBytes / 1024;
-    case "mb":
-      return sizeInBytes / (1024 * 1024);
-    case "gb":
-      return sizeInBytes / (1024 * 1024 * 1024);
-    default:
-      return sizeInBytes;
-  }
+    return emailRegex.test(email);
 }
+
+export const formatDate = (dateString: string) => {
+    return new Date(dateString).toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric'
+    });
+};
