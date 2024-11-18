a3a1e8ce3a3907c0541776660d137b1fdf84d393
Muhammad Said <mohammedabdy10@gmail.com>
2024-11-18 17:19:10 +0300

    feat: add home layout and sidebar components; implement main dashboard structure with data stats

---
+++ apps/web/src/components/features/index.ts
@@ -0,0 +1,4 @@
+export { default as SignupForm } from "./signup-form/signup-form";
+export { default as UsersTable } from "./users-table/users-table";
+export { default as DataStats } from "./data-stats/data-stats";	
+export { default as ImagesTable } from "./images-table/images-table";
