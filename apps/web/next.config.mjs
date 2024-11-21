696b245f3fcaeec8da6facf45a2896e216da474e
mohswell <mohammedabdy10@gmail.com>
2024-11-21 17:15:37 +0300

    feat: update Next.js and Tailwind CSS configurations for improved styling and functionality

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,39 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: process.env.AWS_NEW_BUCKET_DOMAIN || 'new-bucket.com',
-        },
-        {
-          protocol: "https",
-          hostname: process.env.AWS_BUCKET_DOMAIN || 'bucket.com',
-        },
-      ],
-      // domains: [
-      //   ...(process.env.AWS_BUCKET_DOMAIN ? [process.env.AWS_BUCKET_DOMAIN] : []),
-      //   ...(process.env.AWS_NEW_BUCKET_DOMAIN
-      //     ? [process.env.AWS_NEW_BUCKET_DOMAIN]
-      //     : []),
-      // ],
-    },
-    async redirects() {
-      return [
-        {
-          source: "/",
-          destination: "/login",
-          permanent: false,
-        },
-        {
-          source: "/admin",
-          destination: "/admin/login",
-          permanent: true,
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: true,
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: process.env.AWS_BUCKET_DOMAIN || 'bucket.com',
+  //     },
+  //   ],
+  // },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "/",
+  //       permanent: false,
+  //     },
+  //   ];
+  // },
+};
+
+export default nextConfig;
