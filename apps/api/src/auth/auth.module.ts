fd50b495e233be8081bfa435e5610950c82c2be2
mohswell <mohammedabdy10@gmail.com>
2024-11-21 16:15:56 +0300

    feat: integrate PrismaModule and UserProvider into AuthModule

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -5,13 +5,17 @@ import { APP_GUARD } from '@nestjs/core';
 import { JwtGuard } from './providers/guards/jwt.guard';
 import { SessionGuard } from './providers/guards/session.guard';
 import { ThrottlerGuard } from '@nestjs/throttler';
+import { UserProvider } from './providers/suppliers/user.provider';
+import { PrismaModule } from '~factories';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AuthController],
   providers: [
     AuthService,
     JwtGuard,
     SessionGuard,
+    UserProvider,
     {
       provide: APP_GUARD,
       useClass: ThrottlerGuard,
