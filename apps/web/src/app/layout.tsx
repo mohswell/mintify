ccd600d44c8008b8a40740a36176eee362b21c05
mohswell <mohammedabdy10@gmail.com>
2024-11-21 17:17:28 +0300

    feat: add RootLayout component for application structure and theming

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,50 @@
+import type { Metadata } from "next";
+import "../styles/globals.css";
+import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
+
+import { Kanit as FontSans } from "next/font/google";
+
+import { cn } from "../lib/utils";
+import { ThemeProvider } from "../components/views/ui/theme-provider";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
+import Providers from "../providers/Provider";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+  weight: ["200", "400", "500"],
+});
+
+export const metadata: Metadata = {
+  title: "Orcish Fullstack Admin",
+  description: "Fullstack admin application in Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Providers> 
+            <NuqsAdapter>{children}</NuqsAdapter>
+          </Providers>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
