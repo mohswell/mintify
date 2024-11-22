d1fc864ca86cd095098b0b5e43aacdc37441bd82
mohswell <mohammedabdy10@gmail.com>
2024-11-22 16:34:12 +0300

    refactor: update middleware for improved authentication handling and cleanup next.config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,23 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: process.env.AWS_BUCKET_DOMAIN || 'bucket.com',
-      },
-    ],
-  },
-  // async redirects() {
-  //   return [
+  // images: {
+  //   remotePatterns: [
   //     {
-  //       source: "/",
-  //       destination: "/",
-  //       permanent: false,
+  //       protocol: "https",
+  //       hostname: process.env.AWS_BUCKET_DOMAIN || 'bucket.com',
   //     },
-  //   ];
+  //   ],
   // },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
