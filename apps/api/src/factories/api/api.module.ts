8cf6f6c94a45d106893c2f4ef62e1be92f7ae3f5
mohswell <mohammedabdy10@gmail.com>
2024-11-22 16:34:35 +0300

    refactor: add JwtMiddleware to ApiModule for enhanced security

--- apps/api/src/factories/api/api.module.ts
+++ apps/api/src/factories/api/api.module.ts
@@ -3,11 +3,12 @@ import { ApiKeyService } from './api.service';
 import { ApiMiddleware } from '~middleware/api.middleware';
 import { PrismaModule } from '~factories/prisma/prisma.module';
 import { ApiController } from './api.controller';
+import { JwtMiddleware } from '~middleware/jwt.middleware';
 
 @Module({
   imports: [PrismaModule],
-  providers: [ApiMiddleware, ApiKeyService],
-  exports: [ApiMiddleware],
+  providers: [ApiMiddleware, ApiKeyService, JwtMiddleware], 
+  exports: [ApiMiddleware, ApiKeyService, JwtMiddleware],
   controllers: [ApiController],
 })
 export class ApiModule {}
