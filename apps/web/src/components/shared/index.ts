65ea621e8ed27398e6ba3600a09b812095ce4fae
mohswell <mohammedabdy10@gmail.com>
2024-11-22 16:30:48 +0300

    feat: add new shared components (Input, PaginationControls, Textarea, ThemeToggle, Toggle) for enhanced UI functionality

---
+++ apps/web/src/components/shared/index.ts
@@ -0,0 +1,9 @@
+export { Button } from "./Button";
+export { Input } from "./Input";
+export { Filter } from "./Filter";
+export { Dropdown } from "./Dropdown";
+export { DropdownItem } from "./DropdownItem";
+export { Toggle } from "./Toggle";
+export { ThemeToggle } from "./ThemeToggle";
+export { Textarea } from "./Textarea";
+export { Checkbox } from "./Checkbox";
