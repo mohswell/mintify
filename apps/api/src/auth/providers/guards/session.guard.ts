53f95da949b2c53a695cae8ed0b9f09bae2f9db4
mohswell <mohammedabdy10@gmail.com>
2024-11-25 08:59:52 +0300

    feat(auth): enhance session management with validation and deletion methods

--- apps/api/src/auth/providers/guards/session.guard.ts
+++ apps/api/src/auth/providers/guards/session.guard.ts
@@ -1,19 +1,52 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '~factories';
+import { PrismaService } from '~/factories';
 
 @Injectable()
 export class SessionGuard {
   constructor(private readonly prisma: PrismaService) {}
 
-  async createSession(userId: bigint, token: string, expiresAt: Date): Promise<void> {
-    await this.prisma.session.create({
-      data: { userId, token, expiresAt },
+  async createSession(userId: bigint, token: string, expiresAt: Date) {
+    return this.prisma.session.create({
+      data: {
+        userId,
+        token,
+        expiresAt,
+      },
     });
   }
 
-  async deleteExpiredSessions(): Promise<void> {
-    await this.prisma.session.deleteMany({
-      where: { expiresAt: { lte: new Date() } },
+  async validateSession(token: string) {
+    const session = await this.prisma.session.findFirst({
+      where: {
+        token,
+        expiresAt: {
+          gt: new Date(),
+        },
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    if (!session) {
+      return null;
+    }
+
+    return {
+      ...session,
+      userId: session.userId.toString(), // Convert BigInt to string
+      user: {
+        ...session.user,
+        id: Number(session.user.id), // Convert user id to number
+      },
+    };
+  }
+
+  async deleteSession(token: string) {
+    return this.prisma.session.delete({
+      where: {
+        token,
+      },
     });
   }
 }
