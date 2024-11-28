19144b960fa3dd0882005413efffa61a01fcaa89
mohswell <mohammedabdy10@gmail.com>
2024-11-28 17:04:29 +0300

    feat(github): add FileAnalysisService to GithubModule providers

--- apps/api/src/domains/github/github.module.ts
+++ apps/api/src/domains/github/github.module.ts
@@ -3,10 +3,11 @@ import { GithubController } from '~domains/presenters/github/github.controller';
 import { GithubService } from '~domains/presenters/application/github/github.service';
 import { githubProviders } from '~domains/presenters/application/providers/github.provider';
 import { PrismaService } from '~factories';
+import { FileAnalysisService } from '~domains/presenters/sanctum/analysis.service';
 
 @Module({
     imports: [],
     controllers: [GithubController],
-    providers: [GithubService, ...githubProviders, PrismaService],
+    providers: [GithubService, ...githubProviders, PrismaService, FileAnalysisService],
 })
 export class GithubModule { }  
