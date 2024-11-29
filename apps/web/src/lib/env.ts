1fa2041ac9c62188fe8175f9df692561c33bd193
mohswell <mohammedabdy10@gmail.com>
2024-11-29 21:36:12 +0300

    add assertion to auth url for supertype validation

--- apps/web/src/lib/env.ts
+++ apps/web/src/lib/env.ts
@@ -14,3 +14,9 @@ export const SUPABASE_ANON_KEY = assertValue(
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   "Missing environment variable: NEXT_PUBLIC_SUPABASE_ANON_KEY"
 );
+
+
+export const AUTH_REDIRECT_URL = assertValue(
+  process.env.NEXT_PUBLIC_AUTH_REDIRECT_URL || 'https://mintifyapp.vercel.app',
+  "Missing Auth Redirect Url, Taking to the default vercel url"
+);
