1e93848e2afc534aa86849f773fe4b321db1288f
mohswell <mohammedabdy10@gmail.com>
2024-11-30 22:37:24 +0300

    feat(config): update image configuration to allow remote patterns from GitHub avatars

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: process.env.AWS_BUCKET_DOMAIN || 'bucket.com',
-  //     },
-  //   ],
-  // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com"
+        //hostname: process.env.AWS_BUCKET_DOMAIN || 'bucket.com',
+      },
+    ],
+  },
   // async redirects() {
   //   return [
   //     {
