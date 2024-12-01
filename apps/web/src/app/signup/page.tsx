a008af3fcb0e70255b633de04c542e53bc3ba5a8
mohswell <mohammedabdy10@gmail.com>
2024-12-01 13:28:03 +0300

    feat(signup): enhance signup page layout with navigation and footer; update card styling and remove description

--- apps/web/src/app/signup/page.tsx
+++ apps/web/src/app/signup/page.tsx
@@ -6,21 +6,26 @@ import {
   CardTitle,
 } from "@/components/views/ui/card";
 import { SignupForm } from "@/components/features";
+import Footer from "@/components/views/login/Login-Footer";
+import { Nav } from "@/components/views/login/Login-Nav";
 
 export default function Signup() {
   return (
-    <div className="flex justify-center items-center inset-0 absolute">
-      <Card className="mx-auto max-w-sm">
-        <CardHeader>
-          <CardTitle className="text-xl">Sign Up</CardTitle>
-          <CardDescription>
-            Enter your information to create an account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <SignupForm />
-        </CardContent>
-      </Card>
+    <div className="flex flex-col min-h-screen overflow-none">
+      <Nav />
+      <div className="flex flex-grow justify-center items-center p-3">
+        <Card className="mx-auto max-w-md border-none shadow-none">
+          <CardHeader>
+            {/* <CardDescription>
+              Enter your information to create an account
+            </CardDescription> */}
+          </CardHeader>
+          <CardContent>
+            <SignupForm />
+          </CardContent>
+        </Card>
+      </div>
+      <Footer />
     </div>
   );
 }
