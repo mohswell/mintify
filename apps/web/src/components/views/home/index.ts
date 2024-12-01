0365c4ce03410e98a99111239f42c89fcf2af93a
mohswell <mohammedabdy10@gmail.com>
2024-12-01 22:48:30 +0300

    feat: remove unused components related to AI analysis and file display

--- apps/web/src/components/views/home/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as FileList } from './FileList';
-export { default as FileDiff } from './FileDiff';
-export { default as AIDialog } from './AIDialog';
-export { default as Sidebar } from './Sidebar';
-export { default as TokenUsage } from './TokenUsage';
