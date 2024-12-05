fca882d1f4787424d92145f1ce8a2fe5ad4e97eb
mohswell <mohammedabdy10@gmail.com>
2024-12-05 14:28:52 +0300

    chore(eslint): disable unused variable rule in ESLint configuration

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -20,7 +20,8 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-unused-vars': 'off',
+    // '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/no-explicit-any': 'off',
     'prettier/prettier': ['error', {
       "singleQuote": true,
