0c4748cfa7f0c913b672114ab087b7586fcc26e3
mohswell <mohammedabdy10@gmail.com>
2024-12-05 16:50:49 +0300

    chore(api): add comment to ping.ts for clarity on functionality

--- apps/web/src/pages/api/ping.ts
+++ apps/web/src/pages/api/ping.ts
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 type Data = {
     name: string;
 };
-
+//Ping Pong Test
 export default function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>,
