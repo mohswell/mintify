23e76cf7f54b7fea17a8bf3de7e0bebed971c802
mohswell <mohammedabdy10@gmail.com>
2024-12-18 17:03:02 +0300

    fix(gemini): remove TraitsModule as it is no longer needed and causing memory leaks

--- apps/api/src/gemini/traits/traits.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TestExtensionService } from './extensions/extensions.service';
-
-@Module({
-  providers: [TestExtensionService],
-  exports: [TestExtensionService],
-})
-export class TraitsModule {}
